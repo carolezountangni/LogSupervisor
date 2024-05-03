c2b627b5f1d7c40cdec6015c0f752860ec60c012
carolezountangni <czountangni@gmail.com>
2024-05-03 10:31:11 +0200

    first commit

---
+++ src/LogSupervisor.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace carolezountangni\LogSupervisor;
+
+class LogSupervisor
+{
+    /**
+     * Create a new LogSupervisor Instance
+     */
+    public function __construct()
+    {
+        // constructor body
+    }
+}
