d3efa635e2c0cd4061257cf73404eccdc75f9889
carolezountangni <czountangni@gmail.com>
2024-06-03 11:08:03 +0200

    search-7

--- src/Http/Controllers/LogController.php
+++ src/Http/Controllers/LogController.php
@@ -34,6 +34,8 @@ class LogController extends Controller
 
         $backUrl = config('log-supervisor.back_to_system_url');
         $backLabel = config('log-supervisor.back_to_system_label');
+        // Obtenir l'objet Validator
+        $validator = $request->validator();
         // return redirect()->back()->withErrors($validator);
 
         // return view('log-supervisor::index', [
