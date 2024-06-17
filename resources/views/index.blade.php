5e27476dc2958aed52b801cd2a27cf87a649c7d2
carolezountangni <czountangni@gmail.com>
2024-06-17 16:34:11 +0200

    css-6

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,27 +4,17 @@
 
 @section('content')
 
-<div class="custom-container">
-    <div class="custom-row">
-        <div class="custom-col-lg-6 mx-auto">
-            <div class="bg-light p-4 mb-4 rounded shadow-sm custom-menu">
+
+            <div class="bg-light p-5 mb-1 text-center">
                 <form action="" method="get">
-                    <div class="custom-row g-2">
-                        <div class="custom-col-sm">
+                    <div class="container d-flex gap-2">
                             <input type="date" name="created_at" id="created_at" class="form-control mb-1" placeholder="Date" value="{{ $input['created_at'] ?? ''}}">
-                        </div>
-                        <div class="custom-col-sm">
                             <input type="text" name="title" id="title" class="form-control mb-1" placeholder="Mot clé" value="{{ $input['title'] ?? ''}}">
-                        </div>
-                        <div class="custom-col-sm-auto">
-                            <button type="submit" class="btn btn-primary mb-1"><i class="fa fa-search"></i> Rechercher</button>
-                        </div>
+                            <button type="submit" class="btn btn-primary btn-sm flex-grow-0"><i class="fa fa-search"></i> Rechercher</button>
                     </div>
                 </form>
             </div>
-        </div>
-    </div>
-</div>
+
 
 <div class="custom-container">
     <div class="custom-row">
